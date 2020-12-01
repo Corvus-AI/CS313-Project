9311bf8b30afa2fac4dc58a3470f06cc17be0353
Corvus <47604183+Corvus-AI@users.noreply.github.com>
2020-12-01 14:58:35 +0530

    Created table and added data
    I have inserted .sql files also

---
+++ SQL Files/InsertData.sql
@@ -0,0 +1,30 @@
+insert into admin values ('A0001' , 'qwerty');
+insert into admin values ('A0002' , 'qwerty');
+
+
+insert into voter values ('V0001' , 'test1', '1234' , 'test1@gmail.com');
+insert into voter values ('V0002' , 'test2', '1234' , 'test1@gmail.com');
+insert into voter values ('V0003' , 'test3', '1234' , 'test1@gmail.com');
+insert into voter values ('V0004' , 'test4', '1234' , 'test1@gmail.com');
+insert into voter values ('V0005' , 'test5', '1234' , 'test1@gmail.com');
+insert into voter values ('V0006' , 'test6', '1234' , 'test1@gmail.com');
+insert into voter values ('V0007' , 'test7', '1234' , 'test1@gmail.com');
+insert into voter values ('V0008' , 'test8', '1234' , 'test1@gmail.com');
+insert into voter values ('V0009' , 'test9', '1234' , 'test1@gmail.com');
+insert into voter values ('V0010' , 'test10', '1234' , 'test1@gmail.com');
+
+
+insert into survey values ('S0001' , 'Online Sem', ' Question1 with options 1-4' );
+insert into survey values ('S0002' , 'Online Sem', ' Question2 with options 1-4' );
+insert into survey values ('S0003' , 'Online Sem', ' Question3 with options 1-4' );
+
+
+insert into data values ('V0001' , 'S0001' , 1 ) ;
+insert into data values ('V0001' , 'S0002' , 1 ) ;
+insert into data values ('V0001' , 'S0003' , 2 ) ;
+insert into data values ('V0004' , 'S0002' , 1 ) ;
+insert into data values ('V0004' , 'S0001' , 3 ) ;
+insert into data values ('V0002' , 'S0001' , 2 ) ;
+insert into data values ('V0009' , 'S0002' , 2 ) ;
+insert into data values ('V0007' , 'S0003' , 4 ) ;
+insert into data values ('V0008' , 'S0001' , 4 ) ;
---
+++ SQL Files/create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE admin (
+	admin_id VARCHAR(5) PRIMARY KEY,
+	password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE voter (
+    voter_id varchar(5) PRIMARY KEY,
+    vname varchar(55) NOT NULL,
+    password varchar(55) NOT NULL,
+    email varchar(55) NOT NULL
+);
+
+CREATE TABLE survey (
+    survey_id varchar(5) PRIMARY KEY,
+    sname varchar(55) NOT NULL,
+    description varchar(100) NOT NULL 
+);
+
+CREATE TABLE data (
+    v_name VARCHAR(50) ,
+    s_name VARCHAR(50) ,
+    option INT ,
+    PRIMARY KEY (v_name , s_name)
+);
